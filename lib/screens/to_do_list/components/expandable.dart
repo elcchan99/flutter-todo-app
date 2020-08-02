915a2efd0030324efd481382410eaeb378946a29
edgar.chan <edgar.chan@cloverhealth.com>
2020-08-02 16:50:40 +0800

    add expanable

---
+++ lib/screens/to_do_list/components/expandable.dart
@@ -0,0 +1,63 @@
+// Reference: https://stackoverflow.com/questions/49029841/how-to-animate-collapse-elements-in-flutter
+import 'package:flutter/material.dart';
+
+class ExpandedSection extends StatefulWidget {
+  final Widget child;
+  final bool expand;
+  final AnimationController controller;
+  ExpandedSection({Key key, this.expand = false, this.child, this.controller})
+      : super(key: key);
+
+  @override
+  _ExpandedSectionState createState() => _ExpandedSectionState();
+}
+
+class _ExpandedSectionState extends State<ExpandedSection>
+    with SingleTickerProviderStateMixin {
+  AnimationController expandController;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    print("ExpandedSection initState()");
+    prepareAnimations();
+    _runExpandCheck();
+  }
+
+  ///Setting up the animation
+  void prepareAnimations() {
+    expandController = widget.controller ??
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    animation = CurvedAnimation(
+      parent: expandController,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  void _runExpandCheck() {
+    if (widget.expand) {
+      expandController.forward();
+    } else {
+      expandController.reverse();
+    }
+  }
+
+  @override
+  void didUpdateWidget(ExpandedSection oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _runExpandCheck();
+  }
+
+  @override
+  void dispose() {
+    expandController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+        axisAlignment: 1.0, sizeFactor: animation, child: widget.child);
+  }
+}
